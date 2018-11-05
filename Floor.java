5520f7b2a4f452a396732f8ad9cf2eb6be20906a
BelleAA <44402110+BelleAA@users.noreply.github.com>
2018-11-05 13:49:55 +0000

    Update Floor.java
    additional commentary and assigning to fields in Floormap

--- Floor.java
+++ Floor.java
@@ -30,45 +30,43 @@ public class Floor extends FloorMap {
 		wallLayer[2][2] = 5;
 	}
 	
-	// randomly places number 1s on floor representing traps
-	private int[][] createTraps(int difficulty){
-		int[][] traps = new int[100][100];
+	// randomly places number 1s on floor representing traps (1 type of trap)
+	private void createTraps(int difficulty){
+		int[][] trapLayer = new int[100][100];
 		Random rand = new Random();
 
 		while (difficulty > 0){
 			int n = rand.nextInt(100);
 			int m = rand.nextInt(100);
-			traps[n][m] = 1;
+			trapLayer[n][m] = 1;
 		}
 		
-		return traps;
 	}
-	// randomly places numbers 2-5 on floor representing items
-	private int[][] createItems(int difficulty){
-		int[][] items = new int[100][100];
+	// randomly places numbers 2-5 on floor representing items (4 types of item)
+	private void createItems(int difficulty){
+		int[][] itemLayer = new int[100][100];
 		Random rand = new Random();
 
 		while (difficulty > 0){
 			int n = rand.nextInt(100);
 			int m = rand.nextInt(100);
 			int o = rand.nextInt(5) + 2;
-			items[n][m] = o;
+			itemLayer[n][m] = o;
 		}
 		
-		return items;
 	}
-	// randomly places numbers 6-9 on floor representing monsters
-	private int[][] createMonsters(int difficulty){
-		int[][] monsters = new int[100][100];
+	// randomly places numbers 6-9 on floor representing monsters (4 types of random monster)
+	private void createMonsters(int difficulty){
+		int[][] enemyLayer = new int[100][100];
 		Random rand = new Random();
 
 		while (difficulty > 0){
 			int n = rand.nextInt(100);
 			int m = rand.nextInt(100);
 			int o = rand.nextInt(4) + 6;
-			monsters[n][m] = o;
+			enemyLayer[n][m] = o;
 		}
-		return monsters;
+
 	}
 	
 	public void displayGround(){
