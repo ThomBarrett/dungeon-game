38663bd85d8b7b9daac51623417fee337e5ac75e
BelleAA <44402110+BelleAA@users.noreply.github.com>
2018-11-05 13:20:36 +0000

    Update Floor.java
    added createItem, createTrap and createMonster methods

--- Floor.java
+++ Floor.java
@@ -27,6 +27,45 @@ public class Floor extends FloorMap {
 		wallLayer[2][2] = 5;
 	}
 	
+	// randomly places number 1s on floor representing traps
+	private int[][] createTraps(int difficulty){
+		int[][] traps = new int[100][100];
+		Random rand = new Random();
+
+		while (difficulty > 0){
+			int n = rand.nextInt(100);
+			int m = rand.nextInt(100);
+			traps[n][m] = 1;
+		}
+		
+		return traps;
+	}
+	// randomly places numbers 2-5 on floor representing items
+	private int[][] createItems(int difficulty){
+		int[][] items = new int[100][100];
+		Random rand = new Random();
+
+		while (difficulty > 0){
+			int n = rand.nextInt(100);
+			int m = rand.nextInt(100);
+			int o = rand.nextInt(5) + 2;
+			items[n][m] = o;
+		}
+		
+		return items;
+	}
+	// randomly places numbers 6-9 on floor representing monsters
+	private int[][] createMonsters(int difficulty){
+		int[][] monsters = new int[100][100];
+		Random rand = new Random();
+
+		while (difficulty > 0){
+			int n = rand.nextInt(100);
+			int m = rand.nextInt(100);
+			int o = rand.nextInt(4) + 6;
+			monsters[n][m] = o;
+		}
+	
 	public void displayGround(){
 		for(int i = 0; i < this.sizeX; i++){
 			for(int j = 0; j < this.sizeY; j++){
